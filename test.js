ebcfeba3f8e24ac7496888075c7e564f7ea2c494
Spencer <spencerb242@proton.me>
2022-08-31 19:05:35 -0600

    finished fetcher.js

--- test.js
+++ test.js
@@ -10,19 +10,11 @@ const PATH = args[1];
  
 request(URL, (error, response, body) => {
   fs.writeFile(PATH, body, {flag: 'ax'}, err => {
-    if (err.errno === -17) {
-      rl.question('The file already exists, would you like to overwrite it? y/n  ', (answer) => {
-        if(answer === 'y') {
-          fs.writeFile(PATH, body, err => {
-            console.log(`Downloaded and saved ${body.length} bytes to ${PATH}`);
-            rl.close();
-          })
-        } else {
-          rl.close();
-        }
-      })
+    if (err) {
+      console.log(err.errno);
     } else {
       console.log(`Downloaded and saved ${body.length} bytes to ${PATH}`);
+      rl.close();
     }
   });
 });
